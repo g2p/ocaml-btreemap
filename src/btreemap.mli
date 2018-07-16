682b832150e0eadf6e112c79cce96a9a7aa0158b
Gabriel <g2p.code@gmail.com>
2018-07-16 12:59:34 +0200

    Actually require string keys in the OCaml API

--- src/btreemap.mli
+++ src/btreemap.mli
@@ -1,21 +1,21 @@
-type ('key, 'a) t
+type 'a t
 
-val create: unit -> ('key, 'a) t
-val length: ('key, 'a) t -> int
-val is_empty: ('key, 'a) t -> bool
-val clear: ('key, 'a) t -> unit
-val find_opt: ('key, 'a) t -> 'key -> 'a option
-val mem: ('key, 'a) t -> 'key -> bool
-val add: ('key, 'a) t -> 'key -> 'a -> unit
-val remove: ('key, 'a) t -> 'key -> unit
-val iter: ('key, 'a) t -> ('key -> 'a -> unit) -> unit
-val iter_range: ('key, 'a) t -> 'key -> 'key -> ('key -> 'a -> unit) -> unit
-val iter_inclusive_range: ('key, 'a) t -> 'key -> 'key -> ('key -> 'a -> unit) -> unit
-val fold: ('key, 'a) t -> ('key -> 'a -> 'b -> 'b) -> 'b -> 'b
-val exists: ('key, 'a) t -> ('key -> 'a -> bool) -> bool
-val min_binding: ('key, 'a) t -> ('key * 'a) option
-val max_binding: ('key, 'a) t -> ('key * 'a) option
-val find_first_opt: ('key, 'a) t -> 'key -> ('key * 'a) option
-val find_last_opt: ('key, 'a) t -> 'key -> ('key * 'a) option
-val split_off_after: ('key, 'a) t -> 'key -> ('key, 'a) t
+val create: unit -> 'a t
+val length: 'a t -> int
+val is_empty: 'a t -> bool
+val clear: 'a t -> unit
+val find_opt: 'a t -> string -> 'a option
+val mem: 'a t -> string -> bool
+val add: 'a t -> string -> 'a -> unit
+val remove: 'a t -> string -> unit
+val iter: 'a t -> (string -> 'a -> unit) -> unit
+val iter_range: 'a t -> string -> string -> (string -> 'a -> unit) -> unit
+val iter_inclusive_range: 'a t -> string -> string -> (string -> 'a -> unit) -> unit
+val fold: 'a t -> (string -> 'a -> 'b -> 'b) -> 'b -> 'b
+val exists: 'a t -> (string -> 'a -> bool) -> bool
+val min_binding: 'a t -> (string * 'a) option
+val max_binding: 'a t -> (string * 'a) option
+val find_first_opt: 'a t -> string -> (string * 'a) option
+val find_last_opt: 'a t -> string -> (string * 'a) option
+val split_off_after: 'a t -> string -> 'a t
 
